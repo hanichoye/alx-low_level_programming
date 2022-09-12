429c96aa6034c8368bde110faf9bb9b2155002d8
hanichoye <hana36593@gmail.com>
2022-09-12 06:59:54 -0700

    Task 1 fixation

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,9 +17,9 @@ int main(void)
 	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
 	{
-		printf("greater than 5\n");
+		printf ("greater than 5\n");
 	}
-	else if(n % 10 == 0)
+	else if (n % 10 == 0)
 	{
 		printf("0\n");
 	}
