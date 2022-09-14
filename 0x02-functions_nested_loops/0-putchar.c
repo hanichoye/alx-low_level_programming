92c503a16492ed9bf8fa21753becfeb88af69340
hanichoye <hana36593@gmail.com>
2022-09-14 02:08:34 -0700

    fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -9,8 +9,11 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+void num(int r, int n);
+
 #endif
