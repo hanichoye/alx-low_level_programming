42a957f934becc5f7c697dfc7099a119a730b2f3
hanichoye <hana36593@gmail.com>
2022-09-14 04:18:00 -0700

    hot fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H_INCLUDED                           
-#define HOLBERTON_H_INCLUDED                           
+#ifndef MAIN_H                           
+#define MAIN_H                          
                                                        
 int _putchar(char c);                                  
 void print_alphabet(void);                             
@@ -11,8 +11,8 @@ int _abs(int);
 int print_last_digit(int);                             
 void jack_bauer(void);                                 
 void times_table(void);                                
-int add(int a, int b);                                 
+int add(int, int);                                 
 void print_to_98(int n);                               
 void print_times_table(int n);                         
-void num(int r, int n);                                
+#endif                                
                                                        
