a5b6217fb56ba7de58ac3b38fd34718e55ef969b
hanichoye <hana36593@gmail.com>
2022-09-15 05:07:10 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * nul - multiplies two integers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplication.
+ */
+int nul(int a, int b)
+{
+return (a * b);
+}
