f404f65c426893a4acb48bf128bbc9d2b39a8dc5
hanichoye <hana36593@gmail.com>
2022-09-20 06:12:07 -0700

    Tasks

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,27 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * puts2 - puts2 function
- * @str: str parameter
+ * rev_string - reversing a string
+ * @s: parameter s
  */
-void puts2(char *str)
+void rev_string(char *s)
 {
-	int len, i;
+	char tmp;
+	int i, length1, length2;
 
-	len = strlen(str);
-	for (i = 0; i < len; i += 2)
-		_putchar(str[i]);
-	_putchar('\n');
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length1] != '\0')
+		length1++;
+
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
+	}
 }
+
