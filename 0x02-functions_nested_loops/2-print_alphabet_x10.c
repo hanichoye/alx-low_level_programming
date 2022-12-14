de8f2b67b8141f0018cb71131e1e5993a7decf82
hanichoye <hana36593@gmail.com>
2022-12-13 23:51:16 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,22 @@
 
 /**
  * print_alphabet_x10 - function to print abc 10 times
+ * Return: void
  */
-void print_alphabet_x10(void)
-{
-	char c, i;
 
-	for (i = 0; 1 <= 9; i++)
+void print_alphabet_x10(void)
+
+{
+	char c;
+	int i = 0;
+
+	while (i <= 0)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-			_putchar(c);
-		_putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
 	}
 }
