08a53191adaee83a631c5f86a5267e07c042db0e
hanichoye <hana36593@gmail.com>
2022-12-16 03:49:43 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,8 +2,7 @@
 
 /**
  * print_diagonal - Draws a diagonal lines according parameter
- * @n: The number of times to print diagonal lines 
- *
+ * @n: The number of times to print diagonal lines
  * Return: empty
  */
 void print_diagonal(int n)
