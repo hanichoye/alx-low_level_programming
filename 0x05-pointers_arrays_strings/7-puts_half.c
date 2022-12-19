9eb0c3913ffe07a0bd520cbd983a5747e231222a
hanichoye <hana36593@gmail.com>
2022-12-19 00:44:27 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,20 +1,20 @@
-#include <string.h>
+#include "main.h"
 
 /**
  * puts_half - puts half
  * @str: string param
+ * Description: If odd number of chars, print (length - 1) / 2
  */
 void puts_half(char *str)
 {
-	int x, y, i;
+	int i;
 
-	x = strlen(str);
-	if (x % 2 == 1)
-		y = x / 2 + 1;
-	else
-		y = x / 2;
-	for (i = y; i < x; i++)
+	for (i = 0; str[i] != '\0'; i++);
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
 		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
-
