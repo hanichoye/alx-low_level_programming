a53b1e1fc0bafb71ed5ef7484a591b0b6c2265ec
hanichoye <hana36593@gmail.com>
2022-12-22 07:02:51 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,28 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
- * Return - Always 0
+ * main - main block
+ * Description: Write a program that prints all possible
+ * combinations of two two-digit numbers
+ * Return: 0
  */
 int main(void)
 {
-	int i, j;
+	int c = 0;
+	int f_d;
+	int l_d;
 
-	for (i = 0; i <= 99; i++)
+	int c2;
+	int f_d2;
+	int l_d2;
+
+	while (c <= 98)
 	{
-		for (j = 0; j <= 99; j++)
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+		c2 = 0;
+		while (c2 <= 99)
 		{
-			if (j > i)
+			f_d2 = (c2 / 10 + '0');
+			l_d2 = (c2 % 10 + '0');
+
+			if (c < c2)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i != 0)
+				putchar(f_d);
+				putchar(l_d);
+				putchar(' ');
+				putchar(f_d2);
+				putchar(l_d2);
+
+				if (c != 98)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
+			c2++;
 		}
+		c++;
 	}
 	putchar('\n');
 	return (0);
