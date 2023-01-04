76e03918ad498d6527f54a9b102d19f0bef3194d
hanichoye <hana36593@gmail.com>
2023-01-04 12:19:27 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _evaluate - function that returns the
+ * natural square root of a number
+ * @i: integer
+ * @n: integer
+ * Return: evaluate sqrt
+ */
+int _evaluate(int i, int n)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	else if (i * i < n)
+		return (_evaluate(i + 1, n));
+	else if (i * i == n)
+		return (i);
+	return (-1);
+	return (-1);
+}
