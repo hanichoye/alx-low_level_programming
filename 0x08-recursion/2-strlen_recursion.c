17c16a97cd8a1bb09d1de05029254f2382c3158e
hanichoye <hana36593@gmail.com>
2023-01-04 11:11:58 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen_recursion - Return the length of a string.
- * @s: The string to be printed
+ * @s: The string to be measured
  * Return: The length if the string
  */
 int _strlen_recursion(char *s)
