61228b538a6c2cd06236a76fd6e5a41eecabc60a
hanichoye <hana36593@gmail.com>
2023-02-06 03:16:32 -0800

    Fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,5 +27,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	close(o);
 
-	return (0);
+	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,5 +29,5 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	close(o);
 
-	return (0);
+	return (1);
 }
